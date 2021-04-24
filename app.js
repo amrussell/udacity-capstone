639839b422a3ce5ebc421a68d9629807dab93873
Alex Russell <alex.russell.tx@gmail.com>
2021-04-23 23:57:10 -0500

    lol what if this actually worked

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* global require */
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 console.log("Initialized variables, creating server")
