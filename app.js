ef371c118c595c55a822719620e4393f4501b383
Alex Russell <alex.russell.tx@gmail.com>
2021-04-23 22:22:23 -0500

    add lint

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* global require */
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -6,9 +7,9 @@ const port = 3000;
 console.log("Initialized variables, creating server")
 
 http.createServer((req, res) => {
-	  res.statusCode = 200;
-	  res.setHeader('Content-Type', 'text/plain');
-	  res.end('Hello World! This is Alex Russell\'s Udacity capstone!');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World! This is Alex Russell\'s Udacity capstone!');
 }).listen(port, hostname, 511, () => {
     console.log(`Listening at ${hostname}:${port}`);
 });
