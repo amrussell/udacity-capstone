0117ef29f3e9b4887a0ace48e633b7e10d357ec6
Alex Russell <alex.russell.tx@gmail.com>
2021-04-25 21:24:13 -0500

    lint problem

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = 3000;
 
 console.log("Initialized variables, creating server")
 
+var a;
 http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
